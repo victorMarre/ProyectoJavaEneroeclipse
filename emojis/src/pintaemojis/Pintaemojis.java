895c8068da440889956cc85334d19bf614d1f541
victorMarre <122615107+victorMarre@users.noreply.github.com>
2023-03-03 00:37:43 +0100

    Update Pintaemojis.java

--- emojis/src/pintaemojis/Pintaemojis.java
+++ emojis/src/pintaemojis/Pintaemojis.java
@@ -6,7 +6,7 @@ public class Pintaemojis {
 		// imprimir emojis 10 veces 
 		for (int i=0; i<10; i++) {
 			System.out.printf("\uD83D\uDE00");
-			System.out.println("\uD83D\uDE02");
+			System.out.println("\uD83D\uDE03");
 			}
 
 
