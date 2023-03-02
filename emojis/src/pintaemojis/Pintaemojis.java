7d91d43b32261bbb6b45dbcc475a6c96b1e6bec7
victorMarre <vict98908@gmail.com>
2023-03-02 19:26:18 +0100

    trabajo para entornos de desarrollo

---
+++ emojis/src/pintaemojis/Pintaemojis.java
@@ -0,0 +1,14 @@
+package pintaemojis;
+
+public class Pintaemojis {
+
+	public static void main(String[] args) {
+		// imprimir emojis 10 veces 
+		for (int i=0; i<10; i++) {
+			System.out.printf("\uD83D\uDE00");
+			}
+
+
+	}
+
+}
